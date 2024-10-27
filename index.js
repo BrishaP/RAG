30f15bc6725a87f17c3a7103f836c5106dcfa19f
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-10-27 19:41:37 +0000

    creating embedding

---
+++ index.js
@@ -0,0 +1,11 @@
+import openai from './config.js';
+
+/** Create embeddings representing the input text */
+async function main() {
+    const embedding = await openai.embeddings.create({
+      model: "text-embedding-ada-002",
+      input: "The quick brown fox jumped over the lazy dog",
+    });
+    console.log(embedding);
+  }
+  main();
